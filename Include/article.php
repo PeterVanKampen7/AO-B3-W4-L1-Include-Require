75d7dfc8f8ccfabd122be68d9802ef8b1ca22fa4
Peter <99065921@mydavinci.nl>
2021-03-15 11:28:18 +0100

    Include done

---
+++ Include/article.php
@@ -0,0 +1,5 @@
+
+    <img src="davinci.jpg" alt="Da Vinci College">
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta inventore odit iste autem ducimus deserunt dicta veniam laudantium alias sit reiciendis corrupti ab reprehenderit, eum, distinctio, necessitatibus vero eligendi quod?
+    </p>
